fc6917b11514556c3a64d9b07ce2fcbeec6a0b53
contactochiga <contactochiga@gmail.com>
2025-12-24 00:16:06 +0100

    Add createRenderer function to initialize WebGLRenderer

---
+++ src/core/renderer.js
@@ -0,0 +1,9 @@
+import * as THREE from "three";
+
+export function createRenderer() {
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+  document.body.appendChild(renderer.domElement);
+  return renderer;
+}
