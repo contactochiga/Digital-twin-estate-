72678279e4fab129669e150d8d8fa9b973f9ad63
contactochiga <contactochiga@gmail.com>
2025-12-24 10:12:07 +0100

    Update renderer.js

--- src/core/renderer.js
+++ src/core/renderer.js
@@ -1,9 +1,17 @@
+// src/core/renderer.js
 import * as THREE from "three";
 
-export function createRenderer() {
-  const renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setPixelRatio(window.devicePixelRatio);
-  document.body.appendChild(renderer.domElement);
+export function createRenderer(container) {
+  const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: false
+  });
+
+  renderer.setClearColor(0x0b0d11);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+  container.appendChild(renderer.domElement);
+
   return renderer;
 }
