a54541a073b547903312fcd456bba709355b51a2
contactochiga <contactochiga@gmail.com>
2025-12-26 00:20:01 +0100

    Add PLOTS constant for DT Buildings configuration

---
+++ src/estate/Plots.js
@@ -0,0 +1,32 @@
+export const PLOTS = [
+  // ROAD 1 — DT Buildings
+  {
+    id: "DT_R1_L1",
+    type: "DT",
+    road: "ROAD_1",
+    position: { x: -30, z: 60 },
+    rotation: 0
+  },
+  {
+    id: "DT_R1_R1",
+    type: "DT",
+    road: "ROAD_1",
+    position: { x: 30, z: 60 },
+    rotation: Math.PI
+  },
+
+  {
+    id: "DT_R1_L2",
+    type: "DT",
+    road: "ROAD_1",
+    position: { x: -30, z: 120 },
+    rotation: 0
+  },
+  {
+    id: "DT_R1_R2",
+    type: "DT",
+    road: "ROAD_1",
+    position: { x: 30, z: 120 },
+    rotation: Math.PI
+  }
+];
