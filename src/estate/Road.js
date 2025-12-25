bee1e1c1331e27bbff4acb1661ffd60dc4e4aac3
contactochiga <contactochiga@gmail.com>
2025-12-26 00:19:07 +0100

    Add ROADS constant with road configurations

--- src/estate/Road.js
+++ src/estate/Road.js
@@ -1,23 +1,20 @@
-import * as THREE from "three";
-import { createDTBuilding } from "../buildings/DTBuilding";
-
-export function createRoad(id, zOffset) {
-  const road = new THREE.Group();
-  road.name = id;
-
-  const asphalt = new THREE.Mesh(
-    new THREE.PlaneGeometry(50, 200),
-    new THREE.MeshStandardMaterial({ color: 0x444444 })
-  );
-  asphalt.rotation.x = -Math.PI / 2;
-  road.add(asphalt);
-
-  for (let i = 0; i < 6; i++) {
-    const dt = createDTBuilding(`${id}-DT-${i + 1}`);
-    dt.position.set(i % 2 === 0 ? -30 : 30, 0, -80 + i * 30);
-    road.add(dt);
+export const ROADS = [
+  {
+    id: "ROAD_1",
+    position: { x: 0, z: 0 },
+    direction: "NORTH",
+    length: 800
+  },
+  {
+    id: "ROAD_2",
+    position: { x: -120, z: 120 },
+    direction: "NORTH",
+    length: 500
+  },
+  {
+    id: "ROAD_3",
+    position: { x: 120, z: 120 },
+    direction: "NORTH",
+    length: 500
   }
-
-  road.position.z = zOffset;
-  return road;
-}
+];
