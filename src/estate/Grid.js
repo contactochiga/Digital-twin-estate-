424a70afabd8df11a43ce3c76271288f50dc913c
contactochiga <contactochiga@gmail.com>
2025-12-26 00:18:09 +0100

    Add ESTATE_GRID configuration for estate layout

---
+++ src/estate/Grid.js
@@ -0,0 +1,25 @@
+// src/estate/Grid.js
+
+export const ESTATE_GRID = {
+  ORIGIN: { x: 0, y: 0, z: 0 },
+
+  ROAD_WIDTH: 12,
+  WALKWAY: 3,
+  SETBACK: 6,
+
+  BLOCK_SPACING: 18, // distance between facing buildings
+  BUILDING_DEPTH: 24,
+  BUILDING_WIDTH: 20,
+
+  FLOOR_HEIGHT: 3.6,
+
+  PT: {
+    FLOORS: 5,
+    TYPE: "PT"
+  },
+
+  DT: {
+    FLOORS: 3,
+    TYPE: "DT"
+  }
+};
