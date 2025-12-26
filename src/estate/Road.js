13ca20416fd59bba3fd0b443c2d53ad6cb3751bf
contactochiga <contactochiga@gmail.com>
2025-12-26 19:33:12 +0100

    Update Road.js

--- src/estate/Road.js
+++ src/estate/Road.js
@@ -5,19 +5,16 @@ export const ROADS = [
   {
     id: "ROAD_1",
     position: { x: 0, z: 0 },
-    direction: "NORTH",
     length: 800
   },
   {
     id: "ROAD_2",
     position: { x: -120, z: 120 },
-    direction: "NORTH",
     length: 500
   },
   {
     id: "ROAD_3",
     position: { x: 120, z: 120 },
-    direction: "NORTH",
     length: 500
   }
 ];
