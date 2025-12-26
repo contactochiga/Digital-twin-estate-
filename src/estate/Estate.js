bd631068ebe9961fcd34a54cd7bc8dfd472fdd8d
contactochiga <contactochiga@gmail.com>
2025-12-26 19:11:23 +0100

    Refactor estate creation with new building logic

--- src/estate/Estate.js
+++ src/estate/Estate.js
@@ -1,28 +1,57 @@
 // src/estate/Estate.js
 import * as THREE from "three";
+
 import { createPTBuilding } from "../buildings/PTBuilding";
-import { createRoad, ROADS } from "./Road";
+import { createDTBuilding } from "../buildings/DTBuilding";
+
+import { createRoad } from "./Road";
 import { createMEP } from "./MEP";
+import { PLOTS } from "./Plots";
 
 export function createEstate() {
   const estate = new THREE.Group();
   estate.name = "OCH-ESTATE-V1";
 
-  // Roads
-  ROADS.forEach((road) => {
-    estate.add(createRoad(road));
-  });
+  // -------------------------
+  // ROADS
+  // -------------------------
+  estate.add(createRoad("ROAD-1", 0));
+  estate.add(createRoad("ROAD-2", -120));
+  estate.add(createRoad("ROAD-3", -240));
 
-  // PT Buildings
+  // -------------------------
+  // PT BUILDINGS (Apartments)
+  // -------------------------
   const pt1 = createPTBuilding("PT1");
-  pt1.position.set(-20, 0, 0);
+  pt1.position.set(-40, 0, -40);
   estate.add(pt1);
 
   const pt2 = createPTBuilding("PT2");
-  pt2.position.set(20, 0, 0);
+  pt2.position.set(40, 0, -40);
   estate.add(pt2);
 
-  // MEP backbone
+  // -------------------------
+  // DT BUILDINGS (from plots)
+  // -------------------------
+  PLOTS.forEach((plot) => {
+    if (plot.type === "DT") {
+      const dt = createDTBuilding({
+        id: plot.id,
+        position: {
+          x: plot.position.x,
+          y: 0,
+          z: plot.position.z
+        },
+        rotation: plot.rotation
+      });
+
+      estate.add(dt);
+    }
+  });
+
+  // -------------------------
+  // MEP (Estate controlled)
+  // -------------------------
   estate.add(createMEP());
 
   return estate;
